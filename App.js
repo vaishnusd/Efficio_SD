6c68ef3ba27af58d1d72b8a8ad6fb95254b29637
vaishnusd <vaishnu@ehs4safety.com>
2023-11-28 12:45:07 +0530

    For going back

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './src/screens/LoginPage/Login';
-import navigate from './src/navigation/navigate';
 
 export default function App() {
   return (
-    <NavigationContainer>
-    <View style={{ flex: 1 }}>
-    <StatusBar
-  backgroundColor={'#00293A'}  barStyle="light-content" /> 
-      <navigate/>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
-  </NavigationContainer>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
