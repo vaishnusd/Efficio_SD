3e03ed6228ba5bd168ae4ad78b237cfe58addb33
Sumen <sumen@ehs4safety.com>
2023-12-18 09:15:52 +0530

    Made changes in the navigation

--- src/navigation/NavigateMain.js
+++ src/navigation/NavigateMain.js
@@ -74,7 +74,7 @@ export default NavigateMain = () => {
 	return (
 		<NavigationContainer independent={true}>
 			<Stack.Navigator
-				initialRouteName='DrawerNavigator'
+				initialRouteName='Login'
 				screenOptions={{ headerShown: false }}
 			>
 				<Stack.Screen
